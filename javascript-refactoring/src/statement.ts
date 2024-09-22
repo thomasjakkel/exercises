e9ecafa52637fb739b794790f1005e5055393838
thomasjakkel <thomas.jakkel@gmail.com>
2024-09-22 23:31:19 +0200

    Added js Doc to each function. Renamed variables. Moved a variable out of calculate Function

--- javascript-refactoring/src/statement.ts
+++ javascript-refactoring/src/statement.ts
@@ -1,80 +1,52 @@
-import { Invoice, Play } from "./data/types"
-import { Plays } from "./data/types"
-
-const format = (amount: number) => {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format(amount / 100)
-}
+import { Invoice, Play, Plays } from "./data/types"
 
 /**
- *
- * @param invoice   An invoice for each customer with his performances
- * @param plays     Information of all plays to map the playID from the invoice.performances
- * @returns         String with the total invoice order
+ * Generates a total invoice order for a customer with his booked performances
+ * @param {Invoice} invoice   An invoice for each customer with all booked performances
+ * @param {Plays} plays       Information of all plays to map the playID from the invoice.performances
+ * @returns {string}          String with the total invoice order
  */
+
 const statement = (invoice: Invoice, plays: Plays) => {
   let totalAmount = 0
   let volumeCredits = 0
-  let result = `Statement for ${invoice.customer}\n`
+  let statementLog = ""
 
+  // Calculate costs and credits for each play
   invoice.performances.forEach(({ playID, audience }) => {
-    if (!plays[playID]) throw new Error(`${playID} not found in plays`)
+    if (!plays[playID]) {
+      throw new Error(`Performance with Play ID: ${playID} not found in plays`)
+    }
 
     const play = plays[playID]
-    const { playAmount, playResult, playVolumeCredits } = calculatePlayCosts(
-      play,
-      audience
-    )
-    result += playResult
-    totalAmount += playAmount
+    const { playCosts, playVolumeCredits } = calculatePlayCosts(play, audience)
+
+    // Update variables for this play
+    statementLog += `  ${play.name}: ${format(playCosts)} (${audience} seats)\n`
+    totalAmount += playCosts
     volumeCredits += playVolumeCredits
   })
 
-  // for (let perf of invoice.performances) {
-  //   const play = plays[perf.playID]
-  //   let thisAmount = 0
-
-  //   switch (play.type) {
-  //     case "tragedy":
-  //       thisAmount = 40000
-  //       if (perf.audience > 30) {
-  //         thisAmount += 1000 * (perf.audience - 30)
-  //       }
-  //       break
-  //     case "comedy":
-  //       thisAmount = 30000
-  //       if (perf.audience > 20) {
-  //         thisAmount += 10000 + 500 * (perf.audience - 20)
-  //       }
-  //       thisAmount += 300 * perf.audience
-  //       break
-  //     default:
-  //       throw new Error(`unknown type: ${play.type}`)
-  //   }
-
-  //   // add volume credits
-  //   volumeCredits += Math.max(perf.audience - 30, 0)
-  //   // add extra credit for every ten comedy attendees
-  //   if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5)
-
-  //   // print line for this order
-  //   result += `  ${play.name}: ${format(thisAmount)} (${perf.audience} seats)\n`
-  //   totalAmount += thisAmount
-  // }
-  result += `Amount owed is ${format(totalAmount)}\n`
-  result += `You earned ${volumeCredits} credits\n`
-  return result
+  // Add final sum and credits to statement
+  return (
+    `Statement for ${invoice.customer}\n` +
+    `${statementLog}` +
+    `Amount owed is ${format(totalAmount)}\n` +
+    `You earned ${volumeCredits} credits\n`
+  )
 }
 
-export default statement
-
+/**
+ * Calculates costs and credits for a booked play
+ *
+ * @param {Play} play
+ * @param {number} audience Number of the audience in a play
+ * @returns {{ playCosts: number; playVolumeCredits: number }}
+ */
 function calculatePlayCosts(
   play: Play,
   audience: number
-): { playAmount: number; playResult: string; playVolumeCredits: number } {
+): { playCosts: number; playVolumeCredits: number } {
   let thisAmount = 0
   let volumeCredits = 0
   switch (play.type) {
@@ -100,11 +72,18 @@ function calculatePlayCosts(
   // add extra credit for every ten comedy attendees
   if ("comedy" === play.type) volumeCredits += Math.floor(audience / 5)
 
-  // print line for this order
-  const result = `  ${play.name}: ${format(thisAmount)} (${audience} seats)\n`
   return {
-    playAmount: thisAmount,
-    playResult: result,
+    playCosts: thisAmount,
     playVolumeCredits: volumeCredits,
   }
 }
+
+const format = (amount: number) => {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(amount / 100)
+}
+
+export default statement
