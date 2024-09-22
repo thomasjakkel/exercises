c8251409ca690e7f3ac916e6d309bb580299275e
thomasjakkel <thomas.jakkel@gmail.com>
2024-09-22 17:36:52 +0200

    Moved formatter outside the function and divided result directly by 100

--- javascript-refactoring/README.md
+++ javascript-refactoring/README.md
@@ -2,3 +2,5 @@
 
 1. Initalize new node project and add typescript
 2. Structured .json files to ts-objects with types
+3. Added correct types to statement and refactored as arrow function
+4. Moved formatter outside the function and divided result directly by 100
--- javascript-refactoring/src/statement.ts
+++ javascript-refactoring/src/statement.ts
@@ -1,15 +1,18 @@
 import { Invoice } from "./data/types"
 import { Plays } from "./data/types"
 
+const format = (amount: number) => {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(amount / 100)
+}
+
 const statement = (invoice: Invoice, plays: Plays) => {
   let totalAmount = 0
   let volumeCredits = 0
   let result = `Statement for ${invoice.customer}\n`
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID]
@@ -39,12 +42,10 @@ const statement = (invoice: Invoice, plays: Plays) => {
     if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5)
 
     // print line for this order
-    result += `  ${play.name}: ${format(thisAmount / 100)} (${
-      perf.audience
-    } seats)\n`
+    result += `  ${play.name}: ${format(thisAmount)} (${perf.audience} seats)\n`
     totalAmount += thisAmount
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`
+  result += `Amount owed is ${format(totalAmount)}\n`
   result += `You earned ${volumeCredits} credits\n`
   return result
 }
